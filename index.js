d64b114a7c4cfc7baa44a48a7f0736652bae19d9
Reed Keyes <rbkeyes@gmail.com>
2022-12-22 16:20:30 -0800

    generates readme file

--- index.js
+++ index.js
@@ -8,54 +8,89 @@ const questions = [
         type: 'input',
         message: "What is your project title?",
         name: 'title',
-    }, 
-    // {
-    //     type: 'input',
-    //     message: "Enter a description of your project.",
-    //     name: 'Description',
-    // }, {
-    //     type: 'input',
-    //     message: "What are the steps required to install?",
-    //     name: 'Installation',
-    // }, {
-    //     type: 'input',
-    //     message: "What are the instructions for use?",
-    //     name: 'usage',
-    // }, {
-    //     type: 'list',
-    //     message: "Please choose a license for your project.",
-    //     choices: ["MIT", "Apache License 2.0", "GNU GPLv3", "ISC"],
-    //     name: 'license',
-    // }, {
-    //     type: 'input',
-    //     message: "Include contributions guidelines here.",
-    //     name: 'contributing',
-    // }, {
-    //     type: 'input',
-    //     message: "Write tests for your application. Include examples for how to run tests.",
-    //     name: 'tests'
-    // }, {
-    //         type: 'input',
-    //         message: "What is your github username?",
-    //         name: 'gitHub'
-    //     }, {
-    //         type: 'input',
-    //         message: "What is your email address?",
-    //         name: 'email'
-    //     }
+    },
+    {
+        type: 'input',
+        message: "Enter a description of your project.",
+        name: 'description',
+    }, {
+        type: 'input',
+        message: "What are the steps required to install?",
+        name: 'installation',
+    }, {
+        type: 'input',
+        message: "What are the instructions for use?",
+        name: 'usage',
+    }, {
+        type: 'list',
+        message: "Please choose a license for your project.",
+        choices: ["MIT", "Apache License 2.0", "GNU GPLv3", "ISC"],
+        name: 'license',
+    }, {
+        type: 'input',
+        message: "Include contributions guidelines here.",
+        name: 'contributing',
+    }, {
+        type: 'input',
+        message: "Write tests for your application. Include examples for how to run tests.",
+        name: 'tests'
+    }, {
+        type: 'input',
+        message: "What is your github username?",
+        name: 'github'
+    }, {
+        type: 'input',
+        message: "What is your email address?",
+        name: 'email'
+    }
 ];
 
-inquirer.prompt(questions)
-.then((data) => {
-    const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+// TODO: Create a function to write README file
+const generateReadMe = ({ title, description, installation, usage, license, contributing, tests, github, email  }) =>
+`#${title}
 
-    fs.writeFile(README.md, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
+##Table of Contents
+-[Description](#description)
+-[Installation](#installation)
+-[Usage](#usage)
+-[License](#license)
+-[Contributing](#contributing)
+-[Tests](#tests)
+-[Questions](#questions)
+
+## Description
+${description}
+
+##Installation
+${installation}
+
+##Usage
+${usage}
+
+##License
+${license}
+
+##Contributing
+${contributing}
+
+##Tests
+${tests}
+
+##Questions
+Still have questions? Email the developer at ${email}.
+[GitHub Profile](https://github.com/${github})
+`
+
+inquirer
+.prompt(questions)
+
+.then((data) => {
+    const readmeContent = generateReadMe(data);
+
+    fs.writeFile('README.md', readmeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md file!')
     );
   });
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) { }
 
 // // TODO: Create a function to initialize app
 // function init() { }
